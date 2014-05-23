e3cd1d44be71d7aa0a235e2ba47258e10e21df99
Yuval Oren <yuval@n3twork.com>
2014-05-23 14:07:50 -0700

    Updated version to 0.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'yuval@n3twork.com'
 license          'Apache 2.0'
 description      'Installs/Configures druid'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
 
 depends "java"
